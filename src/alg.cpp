91705ee10f915968c7abfb538932b375d26b6483
vlmrtn <nmartin_02@mail.ru>
2025-04-07 01:52:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,10 +12,10 @@ void insertionsort(int* arr, int len) {
 }
 
 int countPairs1(int *arr, int len, int value) {
-  insertionsort(arr, len);
+  insertionSort(arr, len);
   int count = 0;
   for (int i = 0; i < len; i++) {
-    for (int j = 1 + i; j < len; j++) {
+    for (int j = i+1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         count++;
       }
@@ -25,28 +25,26 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int *arr, int len, int value) {
-  insertionsort(arr, len);
-  int count = 0;
-  int left = 0;
-  int right = len - 1;
-
-  while (left < right) {
-    int sum = arr[left] + arr[right];
-    if (sum == value) {
-      count++;
-      int leftvalue = arr[left];
-      int rightvalue = arr[right];
-      while (left < right && arr[left] == leftvalue) {
-        left++;
+insertionSort(arr, len);
+int left = 0;
+int right = len - 1;
+int count = 0;
+while (left < right) {
+  int sum = arr[left] + arr[right];
+  if (sum == value) {
+    count++;
+    int i = 0;
+    while (i < right) {
+      if (arr[i] == value - arr[left]) {
+        count++;
       }
-      while (left < right && arr[right] == rightvalue) {
-        right--;
-      }
-    } else if (sum < value) {
-      left++;
-    } else {
-      right--;
+      i++;
     }
+    left++;
+  } else if (sum < value) {
+    left++;
+  } else {
+    right--;
   }
   return count;
 }
