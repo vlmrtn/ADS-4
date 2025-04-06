67a38c135d8fc3be7038dc4c1370d4a04acde790
vlmrtn <nmartin_02@mail.ru>
2025-04-07 00:45:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
-    for (int j = i + 1; j < len; j++) {
+    for (int j = 1 + i; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         count++;
       }
@@ -11,35 +11,37 @@ int countPairs1(int *arr, int len, int value) {
   return count;
 }
 
-void bubbleSort(int *arr, int len) {
+void bubblesort(int *arr, int len) {
   for (int i = 0; i < len - 1; i++) {
     for (int j = 0; j < len - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
+      if (arr[j] > arr[j + 1] {
         int temp = arr[j];
         arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        arr[j + 1] = temp; 
       }
     }
   }
 }
 
+
 int countPairs2(int *arr, int len, int value) {
-  bubbleSort(arr, len);
+  bubblesort(arr, len);
   int count = 0;
   int left = 0;
   int right = len - 1;
+
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
       count++;
-      int templeft = arr[left];
-      int tempright = arr[right];
-      while (left < right && arr[left] == templeft) {
+      while (left < right && arr[left] == arr[left + 1]){
         left++;
       }
-      while (left > right && arr[right] == tempright) {
+      while (left < right && arr[right] == arr[right - 1]) {
         right--;
       }
+      left++;
+      right--;
     } else if (sum < value) {
       left++;
     } else {
@@ -49,7 +51,7 @@ int countPairs2(int *arr, int len, int value) {
   return count;
 }
 
-int binarySearch(int *arr, int len, int target, int start) {
+int binarysearch(int *arr, int len, int target, int start) {
   int low = start;
   int high = len - 1;
   while (low <= high) {
@@ -65,22 +67,22 @@ int binarySearch(int *arr, int len, int target, int start) {
   return -1;
 }
 
+
 int countPairs3(int *arr, int len, int value) {
-  bubbleSort(arr, len);
+  bubblesort(arr, len);
   int count = 0;
   for (int i = 0; i < len - 1; i++) {
     if (i > 0 && arr[i] == arr[i - 1]) {
       continue;
     }
     int number = value - arr[i];
-    int index = binarySearch(arr, len, number, i + 1);
-    while (index != -1) {
+    int index = binarysearch(arr, len, number, i + 1);
+    if (index != -1) {
       count++;
       while (index + 1 < len && arr[index + 1] == number) {
         count++;
         index++;
       }
-      break;
     }
   }
   return count;
