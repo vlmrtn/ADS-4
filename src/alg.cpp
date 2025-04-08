ebaaa5836471f61ce7f1b5027659853b55653639
vlmrtn <nmartin_02@mail.ru>
2025-04-08 20:00:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,14 @@
 // Copyright 2021 NNTU-CS
-void insertionsort(int* arr, int size) {
-  for (int i = 1; i < size; i++) {
+void insertionSort(int* arr, int len) {
+  for (int i = 1; i < len; i++) {
     int key = arr[i];
     int j = i - 1;
     while (j >= 0 && arr[j] > key) {
       arr[j + 1] = arr[j];
-      j--;
+        j--;
     }
+    arr[j + 1] = key;
   }
-  arr[j + 1] = key;
 }
 
 int binarySearch(int* arr, int left, int right, int value) {
