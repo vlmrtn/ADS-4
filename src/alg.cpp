b63b4d6cdf6f7a7fe103c9edabe4cbbf6a1efa4b
vlmrtn <nmartin_02@mail.ru>
2025-04-09 00:20:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,18 +12,18 @@ void insertionsort(int* arr, int len) {
 }
 
 int binarySearch(int* arr, int left, int right, int value) {
-  while (left <= right) {
-    int mid = left + (right - left) / 2;
-    if (arr[mid] == value) {
-      return mid;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            return mid;
+        }
+        if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
-    if (arr[mid] < value) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
-  return -1;
+    return -1;
 }
 
 int countPairs1(int *arr, int len, int value) {
