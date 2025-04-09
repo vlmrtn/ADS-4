1563d854737901e99fe05478ef976d3c8eca8b03
vlmrtn <nmartin_02@mail.ru>
2025-04-09 22:00:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,16 +1,4 @@
 // Copyright 2021 NNTU-CS
-void insertionsort(int* arr, int len) {
-  for (int i = 1; i < len; i++) {
-    int key = arr[i];
-    int j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-        j--;
-    }
-    arr[j + 1] = key;
-  }
-}
-
 int binarySearch(int* arr, int left, int right, int value) {
     int count = 0;
     int check;
@@ -50,30 +38,19 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int* arr, int len, int value) {
-    insertionsort(arr, len);
-    int left = 0;
-    int right = len - 1;
     int count = 0;
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        if (sum == value) {
-            int countl = 1;
-            int countr = 1;
-            while (left + 1 < right && arr[left] == arr[left + 1]) {
-                countl++;
-                left++;
+    int rboard = len - 1;
+    for (; 1;) {
+        if (arr[rboard] > value) {
+            rboard--;
+        }
+        else { break; }
+    }
+    for (int i = 0; i <= rboard; i++) {
+        for (int j = rboard; j > i; j--) {
+            if (arr[i] + arr[j] == value) {
+                count++;
             }
-            while (right - 1 > left && arr[right] == arr[right - 1]) {
-                countr++;
-                right--;
-            }
-            count += countl * countr;
-            left++;
-            right--;
-        } else if (sum < value) {
-          left++;
-        } else {
-          right--;
         }
     }
     return count;
@@ -81,7 +58,6 @@ int countPairs2(int* arr, int len, int value) {
 
 
 int countPairs3(int* arr, int len, int value) {
-    insertionsort(arr, len);
     int count = 0;
     for (int i = 0; i < len - 1; i++) {
         int num = value - arr[i];
